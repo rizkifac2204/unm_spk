49cbfb24b76fb78b87ac5c5503a097970d9b31bc
Rizki Fahruroji <rizkifac2204@gmail.com>
2023-11-28 02:27:20 +0700

    selesai bagian result dan r/pw, tinggal tabel terakhir

--- components/Utama.js
+++ components/Utama.js
@@ -104,15 +104,27 @@ function Utama() {
     return averageColumnValue * parsedValue;
   };
 
-  const sumColumnResult = (index) => {
-    const keysToSum = ["valuet1", "valuet2", "valuet3", "valuet4", "valuet5"];
-    const sum = keysToSum.reduce(
-      (acc, key) =>
-        acc +
-        (parseFloat(multiplicationColumn(data[index], data[index][key])) || 0),
+  const sumColumnResultReCount = (obj) => {
+    const newArray = keys.map((k, i) => {
+      const toSum = parseFloat(
+        multiplicationColumn(data[i], obj[`value${k.poin}`])
+      );
+
+      return { ...k, toSum };
+    });
+
+    const sum = newArray.reduce(
+      (prev, key) => prev + (parseFloat(key[`toSum`]) || 0),
       0
     );
-    return 0;
+
+    return sum;
+  };
+
+  const rpw = (r, pw) => {
+    const a = parseFloat(r) || 0;
+    const b = parseFloat(pw) || 0;
+    return parseFloat(a / b).toFixed(2);
   };
 
   return (
@@ -122,69 +134,62 @@ function Utama() {
           Aplikasi Sistem Penunjang Keputusan - Kelompok 3
         </h1>
 
-        {/* <ul>
-          {keys.map((item, idx) => (
-            <li key={idx}>{JSON.stringify(item)}</li>
-          ))}
-        </ul>
-        <ul>
-          {data.map((item, idx) => (
-            <li key={idx}>{JSON.stringify(item)}</li>
-          ))}
-        </ul> */}
+        {/* {JSON.stringify(data[0])} */}
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
           <div className="bg-white p-4 shadow-md">
             <p className="font-semibold mb-3">Input Nilai</p>
 
             <div className="container mx-auto">
-              <table className="min-w-full bg-white border border-gray-300">
-                <thead>
-                  <tr>
-                    <th className="py-2 px-4 border-b">Kriteria</th>
-                    {keys.map((item, i) => (
-                      <th key={i} className="py-2 px-4 border-b">
-                        {item.poin}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((item, index) => (
-                    <tr key={index}>
-                      <td className="py-2 px-2 border-b">{item.poin}</td>
+              <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-300">
+                  <thead>
+                    <tr>
+                      <th className="py-2 px-4 border-b">Kriteria</th>
+                      {keys.map((item, i) => (
+                        <th key={i} className="py-2 px-4 border-b">
+                          {item.poin}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.map((item, index) => (
+                      <tr key={index}>
+                        <td className="py-2 px-2 border-b">{item.poin}</td>
 
+                        {keys.map((k, i) => (
+                          <td key={i} className="py-2 px-2 border-b">
+                            <input
+                              className="shadow appearance-none border rounded w-full py-1 px-2 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                              type="number"
+                              value={item[`value${k.poin}`]}
+                              onChange={(e) =>
+                                handleInputChange(
+                                  index,
+                                  `value${k.poin}`,
+                                  e.target.value
+                                )
+                              }
+                            />
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+
+                    <tr>
+                      <td className="py-2 px-4 border-b">Jumlah</td>
                       {keys.map((k, i) => (
-                        <td key={i} className="py-2 px-2 border-b">
-                          <input
-                            className="shadow appearance-none border rounded w-full py-1 px-2 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            type="number"
-                            value={item[`value${k.poin}`]}
-                            onChange={(e) =>
-                              handleInputChange(
-                                index,
-                                `value${k.poin}`,
-                                e.target.value
-                              )
-                            }
-                          />
+                        <td key={i} className="py-2 px-4 border-b">
+                          {parseFloat(
+                            calculateColumnTotal(`value${k.poin}`)
+                          ).toFixed(2)}
                         </td>
                       ))}
                     </tr>
-                  ))}
-
-                  <tr>
-                    <td className="py-2 px-4 border-b">Jumlah</td>
-                    {keys.map((k, i) => (
-                      <td key={i} className="py-2 px-4 border-b">
-                        {parseFloat(
-                          calculateColumnTotal(`value${k.poin}`)
-                        ).toFixed(2)}
-                      </td>
-                    ))}
-                  </tr>
-                </tbody>
-              </table>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
 
@@ -192,81 +197,90 @@ function Utama() {
             <p className="font-semibold mb-3">Normalisasi</p>
 
             <div className="container mx-auto">
-              <table className="min-w-full bg-white border border-gray-300">
-                <thead>
-                  <tr>
-                    {keys.map((item, index) => (
-                      <th key={index} className="py-2 px-4 border-b">
-                        {item.poin}
-                      </th>
-                    ))}
-                    <th className="py-2 px-4 border-b">PW</th>
-                    <th className="py-2 px-4 border-b">P</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((item, index) => (
-                    <tr key={index}>
-                      {keys.map((k, i) => (
-                        <td key={i} className="py-2 px-4 border-b">
-                          {parseFloat(
-                            divisionNLColumn(
-                              `value${k.poin}`,
-                              item[`value${k.poin}`]
-                            )
-                          ).toFixed(2)}
-                        </td>
+              <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-300">
+                  <thead>
+                    <tr>
+                      {keys.map((item, index) => (
+                        <th key={index} className="py-2 px-4 border-b">
+                          {item.poin}
+                        </th>
                       ))}
-                      <td className="py-2 px-4 border-b">
-                        {parseFloat(averageNLColumn(item)).toFixed(2)}
-                      </td>
-                      <td className="py-2 px-4 border-b">
-                        {sortNLColumn(item.poin)}
-                      </td>
+                      <th className="py-2 px-4 border-b">PW</th>
+                      <th className="py-2 px-4 border-b">P</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {data.map((item, index) => (
+                      <tr key={index}>
+                        {keys.map((k, i) => (
+                          <td key={i} className="py-2 px-4 border-b">
+                            {parseFloat(
+                              divisionNLColumn(
+                                `value${k.poin}`,
+                                item[`value${k.poin}`]
+                              )
+                            ).toFixed(2)}
+                          </td>
+                        ))}
+                        <td className="py-2 px-4 border-b">
+                          {parseFloat(averageNLColumn(item)).toFixed(2)}
+                        </td>
+                        <td className="py-2 px-4 border-b">
+                          {sortNLColumn(item.poin)}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-9 bg-white p-4 shadow-md">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="col-span-2 bg-white p-4 shadow-md">
             <div className="container mx-auto">
-              <table className="min-w-full bg-white border border-gray-300">
-                <thead>
-                  <tr>
-                    <th colSpan={5} className="py-2 px-4 border-b"></th>
-                    <th className="py-2 px-4 border-b">Result</th>
-                    <th className="py-2 px-4 border-b">R/PW</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((item, index) => (
-                    <tr key={index}>
-                      {keys.map((k, i) => (
-                        <td key={i} className="py-2 px-4 border-b">
-                          {parseFloat(
-                            multiplicationColumn(
-                              data[i],
-                              item[`value${k.poin}`]
-                            )
-                          ).toFixed(2)}
-                        </td>
-                      ))}
-                      <td className="py-2 px-4 border-b">
-                        {parseFloat(sumColumnResult(index)).toFixed(2)}
-                      </td>
-                      <td className="py-2 px-4 border-b">-</td>
+              <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-300">
+                  <thead>
+                    <tr>
+                      <th colSpan={5} className="py-2 px-4 border-b"></th>
+                      <th className="py-2 px-4 border-b">Result</th>
+                      <th className="py-2 px-4 border-b">R/PW</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {data.map((item, index) => (
+                      <tr key={index}>
+                        {keys.map((k, i) => (
+                          <td key={i} className="py-2 px-4 border-b">
+                            {parseFloat(
+                              multiplicationColumn(
+                                data[i],
+                                item[`value${k.poin}`]
+                              )
+                            ).toFixed(2)}
+                          </td>
+                        ))}
+                        <td className="py-2 px-4 border-b">
+                          {parseFloat(sumColumnResultReCount(item)).toFixed(2)}
+                        </td>
+                        <td className="py-2 px-4 border-b">
+                          {rpw(
+                            sumColumnResultReCount(item),
+                            averageNLColumn(item)
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
 
-          <div className="col-span-3 bg-white p-4 shadow-md">
+          <div className="bg-white p-4 shadow-md">
             <div className="container mx-auto">
               <table className="min-w-full bg-white border border-gray-300">
                 <tbody>
